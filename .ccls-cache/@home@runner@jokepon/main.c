d4029d450a930ce7c06aa9a5ed36089ca928f3e9
rsdsilva <114616294+rsdsilvainf@users.noreply.github.com>
2022-10-13 15:59:35 +0000

    atualização de dado

--- .ccls-cache/@home@runner@jokepon/main.c
+++ .ccls-cache/@home@runner@jokepon/main.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
  int main(){
    //escolha do jogador
-   int escolha_jogador, escolha_computador;
+   int escolha_computador, escolha_jogador;
    do{
-   printf("1 pedra, 2 papel, 3 tesoura\n");
+   do{
+   printf("1 pedra, 2 papel, 3 tesoura, ou -1 para sair\n");
    printf("escolha uma opçao:\n");
     scanf("%d",&escolha_jogador);
-     if(escolha_jogador < 1 || escolha_jogador > 3)
+    
+     if(escolha_jogador == -1){
+      
+     printf("Final do jogo") ; 
+      break;
+     }else if(escolha_jogador < 1 || escolha_jogador > 3) 
      printf("você digitou uma opção inválida :\n\n");
      } while(escolha_jogador < 1 || escolha_jogador > 3);
+
+   if(escolha_jogador == -1){
+    break;
+   }
      
    if(escolha_jogador == 1)
    printf("vc escolheu pedra:\n\n");
@@ -30,23 +40,23 @@
 
    //resultado
    if(escolha_jogador == escolha_computador){
-     printf("Empate");}
+     printf("Empate:\n");}
      
    if(escolha_jogador ==1 && escolha_computador == 3) 
-      printf("você ganhou");
+      printf("você ganhou;\n");
    if  (escolha_jogador == 2 && escolha_computador ==1)
-      printf("você ganhou");
+      printf("você ganhou:\n");
     if   (escolha_jogador ==3 && escolha_computador ==2)
-     printf("você ganhou");
+     printf("você ganhou:\n");
      
    else{
    if(escolha_jogador ==3 && escolha_computador == 1)
-     printf("voçê perdeu");
+     printf("voçê perdeu:\n");
    if  (escolha_jogador == 1 && escolha_computador ==2)
-     printf("voçê perdeu");
+     printf("voçê perdeu:\n");
     if   (escolha_jogador == 2 && escolha_computador ==3)
-     printf("voçê perdeu");}
-    
+     printf("voçê perdeu:\n");}
+     } while(escolha_jogador!=-1);
    }
    
       
